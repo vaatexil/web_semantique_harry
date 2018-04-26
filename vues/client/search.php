c38fc89072bc458ac893a1e58165c9a6575e9c14
Léo Moscardo <leo.sup@hotmail.fr>
2018-04-26 16:25:37 +0200

    align

--- vues/client/search.php
+++ vues/client/search.php
@@ -2,7 +2,7 @@
 <h4 style="font-size:45px;"id="smoky">Entrez le nom de la page à chercher ! </h1>
 
 <!--Make sure the form has the autocomplete function switched off:-->
-<form style="height:1000px;width:100%;" autocomplete="off" action="/action_page.php">
+<form style="text-align:center;height:1000px;width:100%;" autocomplete="off" action="/action_page.php">
   <div class="autocomplete" >
     <input style="height:100px;width:500px;" id="myInput" type="text" name="myCountry" placeholder="Harry Potter">
   </div>
